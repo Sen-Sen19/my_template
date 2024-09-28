b9cf849a9bf00e1a4fab13fa71bd01bf2c081442
Sen-Sen19 <marcneilsen021900@gmail.com>
2024-09-28 12:00:00 +0000

    space

--- page/admin/checkbox.php
+++ page/admin/checkbox.php
@@ -9,6 +9,7 @@
         <div class="col-sm-6">
           <h1 class="m-0">Display</h1>
         </div>
+        
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="view.php">Home</a></li>
